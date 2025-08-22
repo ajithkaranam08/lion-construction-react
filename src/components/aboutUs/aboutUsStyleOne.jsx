5a01811352bfda0e08bce0896e44b854db2e18b3
shahin hussain <shahinhuzzain@gmail.com>
2025-08-22 23:37:20 +0530

    update images

--- src/components/aboutUs/aboutUsStyleOne.jsx
+++ src/components/aboutUs/aboutUsStyleOne.jsx
@@ -22,8 +22,8 @@ function AboutUsStyleOne({ sectionSpace, data }) {
           <Row className={`${data?.reverse ? 'flex-row-reverse' : ''}`}>
             <Col xs={12} lg={6} >
               {data?.image && (
-                <div className="about-us-img-wrap about-img-left h-100">
-                  <img loading="lazy" width={600} height={767} src={`${data?.relativePath ? data?.image : `/img/banner/${data?.image}`}`} alt="About Us Image" />
+                <div className="about-us-img-wrap about-img-left  h-100">
+                  <img loading="lazy" width={600} height={767} src={`${data?.relativePath ? data?.image : `/img/banner/${data?.image}`}`} alt="About Us Image" className="img-align-height"/>
                   {/* <div className="about-us-img-info about-us-img-info-2 about-us-img-info-3">
                   <div className="ltn__video-img ltn__animation-pulse1">
                     <img src="/img/others/8.png" alt="video popup bg image" />
